aec1f54d574886ea9e65888e42663f1d4b835503
ayeeesaket <saketvermawid@gmail.com>
2025-06-15 01:36:48 +0530

    adios amigos final draft 2

--- Frontend/src/components/landingPage/LandingPage.jsx
+++ Frontend/src/components/landingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState  , useEffect} from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { useNavigate } from "react-router-dom";
 import "../../styles/LandingPage.css";
