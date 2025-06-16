4bb7c0dd16a4754bf0878582b71fabcc35fe783d
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-16 11:18:30 +0530

    pushing234

--- Frontend/src/components/Bookingpage/Bookingpage.jsx
+++ Frontend/src/components/Bookingpage/Bookingpage.jsx
@@ -229,7 +229,7 @@ const Bookingpage = () => {
     dispatch({ type: "facilityId/setFacilityId", payload: facilityId });
     console.log("Selected Facility ID1:", facilityId);
     console.log(typeof facilityId);
-    // console.log(token);
+    console.log(token);
     
     try {
       const response1 = await axios.get(
