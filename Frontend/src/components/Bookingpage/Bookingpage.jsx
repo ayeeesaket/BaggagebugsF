5df071e71e660f57ce5162d7825d9ed4046a66e9
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-18 02:43:45 +0530

    updated cashfree28

--- Frontend/src/components/Bookingpage/Bookingpage.jsx
+++ Frontend/src/components/Bookingpage/Bookingpage.jsx
@@ -361,7 +361,7 @@ const getSessionId = async () => {
 
     await cashfreeRef.current.checkout({
       paymentSessionId: sessionId,
-      redirectTarget: "_modal",
+      redirectTarget: "_blank",
     });
 
     await verifyPayment();
