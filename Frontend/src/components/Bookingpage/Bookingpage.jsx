983bca95183d497b8a16d4aebf5959291ff74c87
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-18 01:15:23 +0530

    updated cashfree14

--- Frontend/src/components/Bookingpage/Bookingpage.jsx
+++ Frontend/src/components/Bookingpage/Bookingpage.jsx
@@ -51,7 +51,6 @@ const Bookingpage = () => {
   const facilityId = useSelector((state) => state.facilityId);
   const [token, setToken] = useState(() => localStorage.getItem("token"));
   const [orderId, setOrderId] = useState("");
-  const [sessionId,setSesionId] = useState(""); 
   // const [orderId, setOrderId] = useState("")
   const dispatch = useDispatch();
   //  New state for selected facility ID
@@ -328,8 +327,12 @@ const Bookingpage = () => {
           },
         }
       );
-     console.log(res.data);
-     
+      if (res.data && res.data.payment_session_id) {
+        console.log(res.data);
+        setOrderId(res.data.order_id);
+        return res.data.payment_session_id;
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -371,7 +374,7 @@ console.log(res.data);
 
     const checkoutOptions = {
       paymentSessionId: sessionId,
-      redirectTarget: "_modal", // "_blank" or "_self" if not using modal
+      redirectTarget: "_self", // "_blank" or "_self" if not using modal
     };
 try {
   const result = await cashfree.checkout(checkoutOptions);
