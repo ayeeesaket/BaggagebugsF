c402007976882c1632b398189cac5915022345ca
ayeeesaket <saketvermawid@gmail.com>
2025-06-19 00:07:19 +0530

    pushing

--- Frontend/src/components/Profile/Profile.jsx
+++ Frontend/src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import { useSelector } from "react-redux";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
