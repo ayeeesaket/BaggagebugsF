956cf38a73a2dd326f2444b7abc3bfb27b51347c
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-22 00:30:28 +0530

    changing prod 2

---
+++ Frontend/utills.js
@@ -0,0 +1,5 @@
+const LocalApi = "https://baggagebugs-1.onrender.com/api/v1";
+const ProductionApi = "https://baggagebugs-b53o.onrender.com/api/v1";
+const GoogleApi = "AIzaSyAEOzozYCsDelJTwhv-pOJtxNk69SPgEzo";
+
+export { LocalApi, ProductionApi, GoogleApi };
