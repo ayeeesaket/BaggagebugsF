cbfa8b80d5d2faf54e73b13d2688d324eedc2390
PRATHAM KARMARKAR <137695598+PRATHAMKARMARKAR@users.noreply.github.com>
2025-07-18 09:11:52 +0530

    Delete Frontend/utills.js

--- Frontend/utills.js
+++
@@ -1,5 +0,0 @@
-const LocalApi = "https://baggagebugs-1.onrender.com/api/v1";
-const ProductionApi = "https://baggagebugs-b53o.onrender.com/api/v1";
-const GoogleApi = "AIzaSyDh1BIiow9o_DhtEJdpGLIvPKZYeuc1D9c";
-
-export { LocalApi, ProductionApi, GoogleApi };
